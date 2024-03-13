de1ef4cd61c47b7042c88b59c811d840291c61ed
castberry10 <soc09072@gmail.com>
2024-03-13 15:26:01 +0900

    중복 되는 답안 제거

--- util.cpp
+++ util.cpp
@@ -2,7 +2,19 @@
 #include <stdlib.h>
 
 int util(){
-    srand(time(NULL));
-    int answer = rand() % 1000;
+    int answer;
+    int answerchar[3];
+    
+    while(1){
+        srand(time(NULL));
+        answer = rand() % 1000;
+        answerchar[0] = answer / 100;
+        answerchar[1] = (answer % 100) / 10;
+        answerchar[2] = answer % 10;
+        if(answerchar[0] != answerchar[1] && answerchar[0] != answerchar[2] && answerchar[0] != answerchar[1]){
+            break;
+        }
+    }
+    
     return answer;
 }
